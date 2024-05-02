b6464b0773d0bb66f582ea3a972ad705613c61ee
draemato <97753719+draemato@users.noreply.github.com>
2024-05-02 21:02:04 +0700

    Add ability to make custom sized grid
    This one adds interactivity and changes some code for.. i honestly just
    tired i forgot why and what i did already.

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".btn-grid");
+const CONTAINER_SIDE = 384;
+let firstRunSides = 384 / 16;
+firstRunSides = firstRunSides.toString() + "px";
 
 for (let i = 0; i < 256; i++){
     let div = document.createElement("div");
-    div.addEventListener("mouseenter", () => div.style.backgroundColor = "wheat")
+    div.classList.add("box");
+    div.style.minWidth = firstRunSides;
+    div.style.minHeight = firstRunSides;
+    div.addEventListener("mouseenter", () => div.style.backgroundColor = "wheat");
     container.appendChild(div);
 }
+
+function newGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    let amount = prompt("How many squares per side? Maximum value is 100.");
+
+    if (amount > 100){
+        return alert("Error. Value cant be higher than 100!");
+    } else if (amount <= 0){
+        return alert("Error. 0 or negative values arent accepted.");
+    }
+
+    let sides = 384 / amount;
+    sides = sides.toString() + "px";
+    console.log(sides);
+
+    for (let i = 0; i < amount * amount; i++){
+        let div = document.createElement("div");
+        div.classList.add("box");
+        div.style.minWidth = sides;
+        div.style.minHeight = sides;
+        div.addEventListener("mouseenter", () => div.style.backgroundColor = "wheat");
+        container.appendChild(div);
+    }
+
+    
+    
+}
+
+button.addEventListener("click", newGrid);
