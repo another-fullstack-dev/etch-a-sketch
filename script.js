2b8dc03add1e3f0cf0435d4a6e52e036b4c86177
draemato <97753719+draemato@users.noreply.github.com>
2024-05-02 13:14:33 +0700

    Attach mouseenter eventListener's to divs

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++){
     let div = document.createElement("div");
+    div.addEventListener("mouseenter", () => div.style.backgroundColor = "wheat")
     container.appendChild(div);
 }
