967a478f3500d66222c94daf6d31509500abbb36
draemato <97753719+draemato@users.noreply.github.com>
2024-05-03 10:31:44 +0700

    Each interaction sets random color

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const container = document.querySelector(".container");
 const button = document.querySelector(".btn-grid");
 const CONTAINER_SIDE = 384;
 let firstRunSides = 384 / 16;
-firstRunSides = firstRunSides.toString() + "px";
+    firstRunSides = firstRunSides.toString() + "px";
+
+let rgb;
 
 for (let i = 0; i < 256; i++){
     let div = document.createElement("div");
     div.classList.add("box");
     div.style.minWidth = firstRunSides;
     div.style.minHeight = firstRunSides;
-    div.addEventListener("mouseenter", () => div.style.backgroundColor = "wheat");
+    div.addEventListener("mouseenter", () => div.style.backgroundColor = randomRGB(0, 255));
     container.appendChild(div);
 }
 
@@ -19,7 +21,7 @@ function newGrid(){
     }
 
     let amount = prompt("How many squares per side? Maximum value is 100.");
-    
+
     if (amount > 100){
         return alert("Error. Value cant be higher than 100!");
     } else if (amount <= 0){
@@ -30,16 +32,23 @@ function newGrid(){
     sides = sides.toString() + "px";
 
     for (let i = 0; i < amount * amount; i++){
+        randomRGB(0, 255);
         let div = document.createElement("div");
         div.classList.add("box");
         div.style.minWidth = sides;
         div.style.minHeight = sides;
-        div.addEventListener("mouseenter", () => div.style.backgroundColor = "wheat");
+        div.addEventListener("mouseenter", () => div.style.backgroundColor = randomRGB(0, 255));
         container.appendChild(div);
     }
+}
 
-    
-    
+function randomRGB(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let r = Math.floor(Math.random() * (max - min + 1)) + min;
+    let g = Math.floor(Math.random() * (max - min + 1)) + min;
+    let b = Math.floor(Math.random() * (max - min + 1)) + min;
+    return "rgb(" + r.toString() + ", " + g.toString() + ", " + b.toString() + ")"; 
 }
 
 button.addEventListener("click", newGrid);
